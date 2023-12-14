6c1d159288ca8321e9c88918c4373f305769aa5a
Nitya Somani <116200890+Nitya-Somani@users.noreply.github.com>
2023-12-14 17:34:09 +0000

    Conversation of DataTypes

---
+++ JavaScript-Basics/Conversation_of_DataTypes.js
@@ -0,0 +1,16 @@
+//conversion of datatypes 
+let i = 0 ; 
+console.log(typeof i );
+let j = null ;
+let k = undefined ;
+console.log(typeof(j) + typeof(k)); 
+
+//NAN intro 
+//Conversion of data types 
+const a = null ; 
+console.log(typeof(a));
+console.log(Number(null))
+console.log(String(null))
+console.table([Integer(null),Integer(undefined),Number(null),Number(undefined),String(null),String(undefined)]);
+
+
